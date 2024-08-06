6d72fbe147070fa4a6daf8a55bfd1ea09d848ac4
Nauel <89044685+NauelSerraino@users.noreply.github.com>
2024-08-06 09:09:48 +0200

    fixed etl

--- pipelines/bert_gender_bias/etl.py
+++ pipelines/bert_gender_bias/etl.py
@@ -1,13 +1,16 @@
+
+#%% Imports
+import numpy as np
 import pandas as pd
 import os
+import re
 
-from pipelines.utils.paths import EXTERNAL_DATA_DIR
+import sys
+sys.path.append('/home/nauel/bert_gender_bias')
 
-gendered_words = pd.read_csv(os.path.join(EXTERNAL_DATA_DIR, 'gendered_words.txt'), header=None, sep='\t')
-dictionary = dict(zip(gendered_words[0], gendered_words[1]))
+from pipelines.utils.paths import EXTERNAL_DATA_DIR, INTERIM_DATA_DIR
 
-import pandas as pd
-
+#%% Preprocess WinoBias
 class PreprocessWinoBias:
     def __init__(self, dictionary, keys_to_swap):
         self.dictionary = dictionary
@@ -17,8 +20,9 @@ class PreprocessWinoBias:
         swapped_dict = self._swap_keys_values(self.dictionary, self.keys_to_swap)
         unique_dict = self._remove_duplicates(swapped_dict)
         self.df = self._dict_to_dataframe(unique_dict)
+        self.df = self._melt_and_more()
         return self.df
-
+    
     def _normalize_pair(self, pair):
         return tuple(sorted(pair))
 
@@ -64,7 +68,34 @@ class PreprocessWinoBias:
 
         return df
 
+    def _melt_and_more(self):
+        df = self.df.melt()
+        df['gender_binary'] = np.where(df.variable == 'female', 1, 0)
+        df.columns = ['gender', 'word', 'gender_binary']
+        df = df.sort_values(by='gender_binary', ascending=True).reset_index(drop=True)
+        return df
 
+
+class PreprocessJobs():
+    def __init__(self, df):
+        self.df = df
+    
+    def clean(self):
+        self.df['job_title_clean'] = self.df['job_title'].apply(lambda x: re.sub(r'\b\d+\w*\b', '', x))
+        job_df = pd.DataFrame()
+        job_df['job_title_clean'] = self.df['job_title_clean'].str.split('/').explode().reset_index(drop=True)
+
+        job_df['n_tokens'] = job_df.job_title_clean.apply(lambda x: len(x.split()))
+        job_df = job_df[job_df.n_tokens==1].reset_index(drop=True)
+        job_df.drop_duplicates(subset="job_title_clean", inplace=True)
+        
+        job_df = job_df[job_df['job_title_clean'].str.len() > 3].reset_index(drop=True)
+        job_df.reset_index(drop=True, inplace=True)
+        
+        return job_df
+        
+    
+#%% Gendered Words Dataset
 keys_to_swap = [
     'aunt', 
     'aunts', 
@@ -88,6 +119,39 @@ keys_to_swap = [
     'ms.'
     ]
 
-preprocess = PreprocessWinoBias(dictionary, keys_to_swap)
-df = preprocess.clean()
+gendered_words = pd.read_csv(os.path.join(EXTERNAL_DATA_DIR, 'gendered_words.txt'), header=None, sep='\t')
+dictionary = dict(zip(gendered_words[0], gendered_words[1]))
 
+preprocess = PreprocessWinoBias(dictionary, keys_to_swap)
+df_gendered = preprocess.clean()
+print(df_gendered)
+
+# %% Extra Gendered Words Dataset
+keys_to_swap = ['camerawomen']
+
+gendered_words = pd.read_csv(os.path.join(EXTERNAL_DATA_DIR, 'extra_gendered_words.txt'), header=None, sep='\t')
+dictionary = dict(zip(gendered_words[0], gendered_words[1]))
+
+preprocess = PreprocessWinoBias(dictionary, keys_to_swap)
+df_extra_gendered = preprocess.clean()
+print(df_extra_gendered.head(60))
+    
+
+# %% Save to CSV
+gendered_words_df = pd.concat([df_gendered, df_extra_gendered])
+gendered_words_df = gendered_words_df.sort_values(by='gender_binary', ascending=True).reset_index(drop=True)
+print(gendered_words_df)
+
+gendered_words_df.to_csv(os.path.join(INTERIM_DATA_DIR, 'gendered_words.csv'), index=False, sep="|")
+
+
+# %% Jobs Dataset
+jobs = pd.read_csv(os.path.join(EXTERNAL_DATA_DIR, 'job_titles.txt'), header=None, sep='\t', names=['job_title'])
+preprocess = PreprocessJobs(jobs)
+
+df_jobs = preprocess.clean()
+
+df_jobs.to_csv(os.path.join(INTERIM_DATA_DIR, 'jobs.csv'), index=False, sep="|")
+
+
+# %%
